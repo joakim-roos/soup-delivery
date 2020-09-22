949ec4c9fb3fe1215df284e77bf440801e0e5c14
Joakim Roos <joakim.roos@student.kyh.se>
2020-09-22 23:40:25 +0200

    added styled components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,17 +8,35 @@ import CheckoutPage from '../Checkout'
 import SignInPage from '../SignIn'
 import SignUpPage from '../SignUp'
 
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    yellow_Primary: '#FFCB40',
+    background: '#F5F5F5',
+    font_Primary: '#333333',
+    font_Secondary: '#4F4F4F',
+    font_Gray_Primary: '',
+    font_Gray_Secondary: '',
+  },
+  text: {
+    spacing_Primary: '0.5px',
+    spacing_Button: '0.3px'
+  }
+}
 function App() {
   return (
-    <Router>
-      <main>
-        <Route exact path={ROUTES.MENU} component={MenuPage} />
-        <Route path={ROUTES.CART} component={CartPage} />
-        <Route path={ROUTES.CHECKOUT} component={CheckoutPage} />
-        <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-        <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-      </main>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <main>
+          <Route exact path={ROUTES.MENU} component={MenuPage} />
+          <Route path={ROUTES.CART} component={CartPage} />
+          <Route path={ROUTES.CHECKOUT} component={CheckoutPage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+        </main>
+      </Router>
+    </ThemeProvider>
   )
 }
 
