26a111c74ca308c684ba27c4a8989a42560a4333
Joakim Roos <joakim.roos@student.kyh.se>
2020-09-30 00:35:11 +0200

    sign in form

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useFirebase } from '../Firebase'
+import { useHistory } from 'react-router-dom'
 
-const SignInPage = () => {
+import * as ROUTES from '../../constants/routes'
+
+const INITIAL_STATE = {
+    email: '',
+    password: '',
+    error: null
+}
+
+
+const SignInPage = () => (
+    <SignInForm />
+)
+
+const SignInForm = () => {
+    const [input, setInput] = useState(INITIAL_STATE)
+    const [error, setError] = useState(null)
+    const firebase = useFirebase()
+    const history = useHistory()
+
+    const isInvalid = input.password === '' || input.email === '';
+
+    const onSubmit = e => {
+        const { email, password } = input
+
+        firebase
+            .doSignInWithEmailAndPassword(email, password)
+            .then(() => {
+                setInput({ ...INITIAL_STATE })
+                history.push(ROUTES.MENU)
+            })
+            .catch(error => {
+                setError(error)
+            })
+
+        e.preventDefault()
+
+    }
+
+    const onChange = e => {
+        setInput({ ...input, [e.target.name]: e.target.value })
+    }
+
     return (
-        <div>
-            Sign In Page
-        </div>
+        <form onSubmit={(e) => onSubmit(e)}>
+
+            <input
+                name="email"
+                value={input.email}
+                onChange={(e) => onChange(e)}
+                type="text" placeholder="Email Address"
+            />
+
+            <input
+                name="password"
+                value={input.password}
+                onChange={(e) => onChange(e)} type="password"
+                placeholder="Password"
+            />
+
+            <button disabled={isInvalid} type="submit">
+                Sign In
+            </button>
+
+            {error && <p>{error.message}</p>}
+
+        </form>
     )
 }
 
